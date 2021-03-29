bdba7c8445b23e300468724fb306ca1e87fb5493
Nidhal-Campus-Num <nidhal.hamila@le-campus-numerique.fr>
2021-03-29 21:53:54 +0200

    fin des exos de tri

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ function swap(tab, i, j) {
 // console.log("après tri : ", tab);
 
 //========================Tri par insertion =======================//
+/* On considère le premier élément comme une liste triée de longueur 1. On range ensuite le
+second élément pour constituer une liste triée de longueur 2, puis on range le troisième élément
+pour avoir une liste triée de longueur 3 et ainsi de suite...
+Le principe du tri par insertion est donc d'insérer à la nième itération le nième élément à la bonne
+place. */
 
 // function triInsertion(tab) {
 //     let n = tab.length;
@@ -43,11 +48,9 @@ function swap(tab, i, j) {
 // console.log(tab);
 //========================Tri par sélection =======================//
 
-// function swap(tab, i, j) {
-//     const temp = tab[i];
-//     tab[i] = tab[j];
-//     tab[j] = temp;
-// }
+/* l'idée du tri par sélection est de prendre le plus petit élément dans le tableau 
+pour le mettre en premier, ensuite de reprend à partie du deuxième élément, et on recommence
+l'opération, jusqu'à la fin. */
 
 // function triSelection(tab) {
 //     const n = tab.length;
@@ -67,24 +70,33 @@ function swap(tab, i, j) {
 
 //========================Tri par bulles =======================//
 
+/* Le principe du tri à bulles est de comparer 2 à 2 les éléments consécutifs d'un tableau 
+et de les permuter si le 1er élément est plus petit que le second.
+Le tri s'arrête quand il n'y a plus de permutation. */
 
-// function triBulle(tab) {
-//     const n = tab.length;
-//     let permut = true;
-//     while (permut) {
-//         permut = false;
-//         for (let i = 0; i < n - 1; i++) {
-//             if (tab[i] > tab[i + 1]) {
-//                 swap(tab, i, i + 1);
-//                 permut = true;
-//             }
-//         }
-//     }
-// }
-// triBulle(tab);
-// console.log(tab);
+function triBulle(tab) {
+    const n = tab.length;
+    let permut = true;
+    while (permut) {
+        permut = false;
+        for (let i = 0; i < n - 1; i++) {
+            if (tab[i] > tab[i + 1]) {
+                swap(tab, i, i + 1);
+                permut = true;
+            }
+        }
+    }
+}
+triBulle(tab);
+console.log(tab);
 
 //========================Tri par fusion =======================//
+
+/* On pourrait l'appeler le tri de César.
+Le tri par fusion :
+-divise la liste à trier en 2
+-trie chacune d'elles (en divisant encore par 2)
+-fusionne les 2 moitiés de liste en une */
 
 // function triFusion(tab) {
 //     // tab = [...tab];
@@ -124,24 +136,36 @@ function swap(tab, i, j) {
 
 //========================Tri rapide =======================//
 
-function triRapide(tab, first = 0, last = tab.length) {
-    tab = [...tab];
-    if (first < last) {
-        pi = partitionner(tab, first, last);
-        triRapide(tab, first, pi - 1);
-        triRapide(tab, pi + 1, last);
-    }
-}
+/* le tr irapide est considèré comme le plus efficace, car son principe est
+ d'ordonner le vecteur en cherchant son pivot pour s'en servir pour réorganiser les éléments de la liste.
+ On prend, d'aileurs souvent le dernier élément du tableau.
+ On permute ces élémentsde façon à ce que pour j, ts les éléments inférieurs soit ds le tab (j+1).
+ On mets ensuite le pivot à la position j.
+ On tri ensuite, de façon récursive la partie dont les éléments st inférieurs au pivot
+ et idem pour les éléments sup au pivot */
 
-function partitionner(tab, first, last) {
-    let j = first;
-    for (i = first; i < last - 1; i++) {
-        if (tab[i] <= last) {
-            swap(tab, i, j);
-            j = j + 1;
-        }
-    }
-    swap(tab, last, j);
-    return j;
-}
-console.log(triRapide(tab));
+// function triRapide(tab, first = 0, last = tab.length - 1) {
+//     // tab = [...tab];
+//     let tabSort = tab;
+//     if (first < last) {
+//         pi = last;
+//         pi = partitionner(tabSort, first, last, pi);
+//         triRapide(tabSort, first, pi - 1);
+//         triRapide(tabSort, pi + 1, last);
+//     }
+//     return tabSort;
+// }
+
+// function partitionner(tab, first, last, pivot) {
+//     swap(tab, pivot, last);
+//     let j = first;
+//     for (i = first; i < last - 1; i++) {
+//         if (tab[i] <= tab[last]) {
+//             swap(tab, i, j);
+//             j = j + 1;
+//         }
+//     }
+//     swap(tab, last, j);
+//     return j;
+// }
+// console.log(triRapide(tab));
